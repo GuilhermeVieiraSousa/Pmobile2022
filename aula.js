69f7140bb014e5555021f173741f158944230209
Guilherme Vieira <guilherme.v.sousa@hotmail.com>
2022-03-04 18:21:51 -0300

    Aula 03

--- aula.js
+++ aula.js
@@ -47,13 +47,106 @@
 // console.log(nome)
 // nome[0]="nome" //n pode
 
-const nomes = ["ana", "maria", "jorge", "atila", "ramires"]
+// const nomes = ["ana", "maria", "jorge", "atila", "ramires"]
 
-// const resultante =  nomes.filter((n) => {
-//     return n.startsWith("")
-// })
+// // const resultante =  nomes.filter((n) => {
+// //     return n.startsWith("")
+// // })
 
-const res = nomes.map((nome) => nome.length)
-console.log(res)
+// const res = nomes.map((nome) => nome.length)
+// console.log(res)
 
-// console.log(resultante)
+// // console.log(resultante)
+
+
+// function hello (){
+//     console.log("oi")
+// }
+
+// function hello (){
+//     console.log("hello, " + nome) 
+// }
+
+// hello ("jõao")
+// function soma (a, b){
+//     return a+b
+// }
+
+// const dobro = function (n){
+//     return 2 * n
+// }
+
+// const triplo = function (n=1){
+//     return n * 3
+// }
+
+//arrow functions
+
+// const hello = () => console.log("Hello")
+
+// const dobro = (n) => n * 2
+
+// console.log(soma(0,0))
+
+//JSON
+
+// let pessoa = {
+//     nome: "João",
+//     idade: 17
+// }
+
+// console.log (pessoa.nome)
+// console.log (pessoa['idade'])
+
+// let pessoaComEndereco = {
+//     nome: "Matria",
+//     idade: 22,
+//     endereco:{
+//         logradouro: "Rua B",
+//         numero: 12
+//     }, 
+//     veiculos: [
+//         {
+//             marca: 'ford',
+//             modelo: 'ka',
+//             ano: 2012,
+//             revisoes: [
+//                 {
+//                     data:'12/02/2013',
+//                     consiltor: 'andre'
+//                 }
+//             ]
+//         },
+//         {
+//             marca: 'volks',
+//             modelo: 'Nivus',
+//             ano: 2020,
+//             revisoes: [
+//                 {
+//                     data:'12/02/2013',
+//                     consiltor: 'jorge'
+//                 }
+//             ]
+//         },
+//         {
+//             marca: 'Chevrolet',
+//             modelo: 'Onix',
+//             ano: 2021,
+//             revisoes: [
+//                 {
+//                     data:'12/02/2013',
+//                     consiltor: 'pedro'
+//                 }
+//             ]
+//         }
+//     ]
+// }
+
+// let calc = {
+//     soma: (a, b) => a + b,
+//     subtracao: function (a, b){
+//         return a - b
+//     }
+// }
+// console.log (calc.soma(2, 3))
+// console.log (coal.subtracao (5, 4))
