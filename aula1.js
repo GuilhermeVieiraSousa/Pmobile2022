bb9bb687c74db2c1e689948e5cb03e963851730f
Guilherme Vieira <guilherme.v.sousa@hotmail.com>
2022-03-11 18:12:18 -0300

    Configuando dotenv

--- aula1.js
+++ aula1.js
@@ -1,1 +1,6 @@
 //axios 
+
+// const url = "https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=a559f2a5392564508e41204076ac34e3" 
+
+require('dotenv').config()
+console.log(process.env.API_KEY)
